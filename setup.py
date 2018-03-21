4bc2cc2f45bd9b9d61035beb18872b5face6840e
Scott White <whitews@gmail.com>
2018-03-21 15:14:46 -0400

    bump version to 0.6

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ logicle_extension = Extension(
 
 setup(
     name='FlowUtils',
-    version='0.5',
+    version='0.6',
     packages=['flowutils'],
     package_data={'': []},
     description='Flow Cytometry Standard Utilities',
