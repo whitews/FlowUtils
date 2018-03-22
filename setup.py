20c613c4ac2414aa5d2161f4a0ee4b19e40ebf7a
Scott White <whitews@gmail.com>
2018-03-22 13:52:12 -0400

    update setup.py to use setuptools to make it pip friendly, bump version

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
-from distutils.core import setup, Extension
-from numpy import get_include
+from setuptools import setup, Extension
+try:
+    from numpy import get_include
+except ImportError:
+    raise RuntimeError(
+        "NumPy is required to build the C extension in FlowUtils")
+
 
 logicle_extension = Extension(
     'flowutils.logicle_c',
@@ -7,18 +12,16 @@ logicle_extension = Extension(
         'flowutils/logicle_c_ext/_logicle.c',
         'flowutils/logicle_c_ext/logicle.c'
     ],
-    include_dirs=[get_include()]
+    include_dirs=[get_include(), 'flowutils/logicle_c_ext']
 )
 
 setup(
     name='FlowUtils',
-    version='0.6',
+    version='0.6.6',
     packages=['flowutils'],
     package_data={'': []},
     description='Flow Cytometry Standard Utilities',
+    license='BSD',
     ext_modules=[logicle_extension],
-    requires=[
-        'numpy (>=1.7)',
-        'scipy (>=0.12)'
-    ]
+    install_requires=['numpy>=1.7']
 )
