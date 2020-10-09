47ca801b049527e7c3db4880897e83381c940511
whitews <whitews@gmail.com>
2020-10-09 11:05:01 -0400

    release version 0.8.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ logicle_extension = Extension(
 
 setup(
     name='FlowUtils',
-    version='0.8.0b',
+    version='0.8.0',
     packages=['flowutils'],
     package_data={'': []},
     description='Flow Cytometry Standard Utilities',
