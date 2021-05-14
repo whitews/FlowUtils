50224cd43e14493cd4f0ef95fd309bb8cdb981ea
whitews <whitews@gmail.com>
2021-05-14 14:56:07 -0400

    pep8 updates to new setup, prep new version

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, Extension
 
-class get_numpy_include(object):
+
+class NumpyInclude(object):
     def __str__(self):
         import numpy
         return numpy.get_include()
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -14,13 +16,13 @@ logicle_extension = Extension(
         'flowutils/logicle_c_ext/_logicle.c',
         'flowutils/logicle_c_ext/logicle.c'
     ],
-    include_dirs=[get_numpy_include(), 'flowutils/logicle_c_ext'],
+    include_dirs=[NumpyInclude(), 'flowutils/logicle_c_ext'],
     extra_compile_args=['-std=c99']
 )
 
 setup(
     name='FlowUtils',
-    version='0.9.2',
+    version='0.9.3b',
     packages=['flowutils'],
     package_data={'': []},
     description='Flow Cytometry Standard Utilities',
