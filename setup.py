8e8473edc5c06753d1182aee1091530fb9420a93
whitews <whitews@gmail.com>
2021-05-20 10:44:25 -0400

    add .eggs to gitignore & bump to next beta version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ logicle_extension = Extension(
 
 setup(
     name='FlowUtils',
-    version='0.9.3',
+    version='0.9.4b0',
     packages=['flowutils'],
     package_data={'': []},
     description='Flow Cytometry Standard Utilities',
