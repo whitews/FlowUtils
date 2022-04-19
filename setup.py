cc077e47e48da30925e26d01f4849c980741e075
whitews <whitews@gmail.com>
2022-04-19 15:26:45 -0400

    bump min numpy version to 1.20

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, Extension, dist
 
 # NumPy is needed to build
 # This retrieves a version at build time compatible with run time version
-dist.Distribution().fetch_build_eggs(['numpy>=1.19'])
+dist.Distribution().fetch_build_eggs(['numpy>=1.20'])
 
 # override inspection for import not at top of file
 # this has to be imported here, after fetching the NumPy egg
@@ -36,7 +36,7 @@ gating_extension = Extension(
 
 setup(
     name='FlowUtils',
-    version='1.0.0b',
+    version='1.0.0b0',
     packages=['flowutils'],
     package_data={'': []},
     description='Flow Cytometry Standard Utilities',
@@ -47,7 +47,7 @@ setup(
     license='BSD',
     url="https://github.com/whitews/flowutils",
     ext_modules=[logicle_extension, gating_extension],
-    install_requires=['numpy>=1.19'],
+    install_requires=['numpy>=1.20'],
     classifiers=[
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.8',
