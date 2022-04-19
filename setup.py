d7283ade9b4bcea74c8368eca7cbe303b93dd266
whitews <whitews@gmail.com>
2022-04-19 15:38:46 -0400

    back to min numpy 1.19

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, Extension, dist
 
 # NumPy is needed to build
 # This retrieves a version at build time compatible with run time version
-dist.Distribution().fetch_build_eggs(['numpy>=1.20'])
+dist.Distribution().fetch_build_eggs(['numpy>=1.19'])
 
 # override inspection for import not at top of file
 # this has to be imported here, after fetching the NumPy egg
@@ -47,7 +47,8 @@ setup(
     license='BSD',
     url="https://github.com/whitews/flowutils",
     ext_modules=[logicle_extension, gating_extension],
-    install_requires=['numpy>=1.20'],
+    setup_requires=['numpy>=1.19'],
+    install_requires=['numpy>=1.19'],
     classifiers=[
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.8',
