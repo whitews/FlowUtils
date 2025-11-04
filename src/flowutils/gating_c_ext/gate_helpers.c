f22e30c047e7c401c5030eb4b161caeb2bc4435d
Scott White <whitews@gmail.com>
2025-11-04 10:50:10 -0500

    replace tab indents w/spaces

--- src/flowutils/gating_c_ext/gate_helpers.c
+++ src/flowutils/gating_c_ext/gate_helpers.c
@@ -15,16 +15,16 @@ double point_is_left(
 }
 
 int calc_wind_count(double point_x, double point_y, int vert_count, double *poly_vertices) {
-	int wind_count = 0;
-	double vert_a_x;
-	double vert_a_y;
-	double vert_b_x;
-	double vert_b_y;
-	double is_left;
+    int wind_count = 0;
+    double vert_a_x;
+    double vert_a_y;
+    double vert_b_x;
+    double vert_b_y;
+    double is_left;
 
     // loop through all edges of the polygon
     for (int i=0; i<vert_count; i++) {
-        //edge from poly_vertices[i] to poly_vertices[i+1]
+        // edge from poly_vertices[i] to poly_vertices[i+1]
         vert_a_x = poly_vertices[(i * 2) + 0];
         vert_a_y = poly_vertices[(i * 2) + 1];
 
